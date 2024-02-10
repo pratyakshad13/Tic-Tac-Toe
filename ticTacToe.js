a5b4dd8621a3c5cbf4edebb0d89bdbd04857d69a
Pratyaksha Dwivedi <pratyakshad13@gmail.com>
2024-02-10 22:47:20 +0530

    draw logic added

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,6 +1,6 @@
 let boxes= document.querySelectorAll(".box");
 let resetBtn= document.querySelector("#reset-btn");
-let newButton=document.querySelector("#new-game");
+let newGame=document.querySelector("#new-game");
 let msgContainer=document.querySelector(".msg-container");
 let msg=document.querySelector("#winner");
 
@@ -12,9 +12,11 @@ const winPattern=[
     [3,4,5],
     [6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
 ]
-
+let count=0;
+let winnerFound=false;
 boxes.forEach((box) => {
     box.addEventListener("click",()=>{
+        count++;
         console.log("clicked!");
         if(turnA){ 
             box.innerText="O";
@@ -26,6 +28,10 @@ boxes.forEach((box) => {
         box.disabled=true;
 
         checkWinner();
+        if(count==9 && !winnerFound){
+            showTie();
+            msgContainer.classList.remove("hide");
+        }
     });
 })
 
@@ -34,11 +40,13 @@ const resetGame=()=>{
     enableButton();
     boxes.forEach((box)=>{
         box.innerText="";
-    })
+    });
+    msgContainer.classList.add("hide");
 }
 
 
 resetBtn.addEventListener("click",resetGame);
+newGame.addEventListener("click",resetGame);
 
 
 
@@ -58,30 +66,45 @@ const enableButton=()=>{
     })
 }
 const showWinner=(Winner)=>{
-    
-    
     msg.innerText=`Congratulations Winner is ${Winner}`
     msgContainer.classList.remove("hide");
     
 }
 
+const showTie=()=>{
+    msg.innerText=`Oops!! It's a Tie!!`
+    msgContainer.classList.remove("hide");
+}
+
+
+newGame.addEventListener("click",()=>{
+    resetGame();
+    msgContainer.classList.add("hide");
+})
+
 
 
 const checkWinner=()=>{
+    let pos1,pos2,pos3;
     for(let pattern of winPattern )//here pattern is bacically the array
     {
+
+        pos1=pattern[0];
+        pos2=pattern[1];
+        pos3=pattern[2];
         // console.log(pattern[0],pattern[1],pattern[2])
         // console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]]) boxes[pattern[0]] denotes the button at pattern[0]th position
-        let patt1=boxes[pattern[0]].innerText;
-        let patt2=boxes[pattern[1]].innerText;
-        let patt3=boxes[pattern[2]].innerText;
-    
-    if(patt1!="" &&patt2!="" &&patt3!=""){
-        if(patt1===patt2 && patt2===patt3){
-            disableButton();
-            showWinner(patt1);
+         patt1=boxes[pattern[0]].innerText;
+         patt2=boxes[pattern[1]].innerText;
+         patt3=boxes[pattern[2]].innerText;
+          if(patt1!="" &&patt2!="" &&patt3!=""){
+            if(patt1===patt2 && patt2===patt3){
+                disableButton();
+                showWinner(patt1);
+                winnerFound=true;
+            }
         }
     }
-}
+    
 }
 
